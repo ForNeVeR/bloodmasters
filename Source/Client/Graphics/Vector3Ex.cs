ae3703618039c414b457ad119e0598b964cbcd9f
Friedrich von Never <friedrich@fornever.me>
2023-07-12 23:38:28 +0200

    (#15) Vector3Ex: remove a TODO comment

--- Source/Client/Graphics/Vector3Ex.cs
+++ Source/Client/Graphics/Vector3Ex.cs
@@ -6,7 +6,6 @@ internal static class Vector3Ex
 {
     public static Vector3 Project(this Vector3 vector, Viewport viewport, Matrix projection, Matrix view, Matrix world)
     {
-        // TODO[#15]: Verify this combination!
         var worldViewProjection = Matrix.Multiply(Matrix.Multiply(world, view), projection);
         return Vector3.Project(
             vector,
