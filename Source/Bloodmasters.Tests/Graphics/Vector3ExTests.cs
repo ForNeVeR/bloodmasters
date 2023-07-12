87499300b36131352390f6ed8f26e90ae4fd8460
Friedrich von Never <friedrich@fornever.me>
2023-07-12 22:58:33 +0200

    Vector3ExTests: update the code style

--- Source/Bloodmasters.Tests/Graphics/Vector3ExTests.cs
+++ Source/Bloodmasters.Tests/Graphics/Vector3ExTests.cs
@@ -1,133 +1,127 @@
 using CodeImp.Bloodmasters.Client.Graphics;
 using SharpDX;
 
-namespace Bloodmasters.Tests.Graphics
+namespace Bloodmasters.Tests.Graphics;
+
+public class Vector3ExTests
 {
-    public class Vector3ExTests
+    private static Matrix GetEmptyMatrix() => new Matrix();
+    private static Matrix GetMatrixWithNormalColumns1()
     {
-        private Matrix GetEmptyMatrix() => new Matrix();
-        private Matrix GetMatrixWithNormalColomns1()
+        var matrix = new Matrix
         {
-            Matrix matrix = new Matrix();
+            Column1 = new Vector4(4f, 3f, 2f, 6f),
+            Column2 = new Vector4(4f, 5f, 2f, 7f),
+            Column3 = new Vector4(3f, 1f, 2f, 6f),
+            Column4 = new Vector4(2f, 6f, 1f, 9f)
+        };
 
-            matrix.Column1 = new Vector4(4f, 3f, 2f, 6f);
-            matrix.Column2 = new Vector4(4f, 5f, 2f, 7f);
-            matrix.Column3 = new Vector4(3f, 1f, 2f, 6f);
-            matrix.Column4 = new Vector4(2f, 6f, 1f, 9f);
-
-            return matrix;
-        }
-        private Matrix GetMatrixWithNormalColomns2()
+        return matrix;
+    }
+    private static Matrix GetMatrixWithNormalColumns2()
+    {
+        var matrix = new Matrix
         {
-            Matrix matrix = new Matrix();
+            Column1 = new Vector4(6f, 2f, 1f, 3f),
+            Column2 = new Vector4(4f, 3f, 3f, 4f),
+            Column3 = new Vector4(5f, 3f, 1f, 4f),
+            Column4 = new Vector4(2f, 3f, 8f, 6f)
+        };
 
-            matrix.Column1 = new Vector4(6f, 2f, 1f, 3f);
-            matrix.Column2 = new Vector4(4f, 3f, 3f, 4f);
-            matrix.Column3 = new Vector4(5f, 3f, 1f, 4f);
-            matrix.Column4 = new Vector4(2f, 3f, 8f, 6f);
-
-            return matrix;
-        }
-        private Matrix GetMatrixWithNormalColomns3()
+        return matrix;
+    }
+    private static Matrix GetMatrixWithNormalColumns3()
+    {
+        var matrix = new Matrix
         {
-            Matrix matrix = new Matrix();
+            Column1 = new Vector4(7f, 5f, 3f, 6f),
+            Column2 = new Vector4(3f, 4f, 5f, 9f),
+            Column3 = new Vector4(2f, 1f, 2f, 3f),
+            Column4 = new Vector4(4f, 5f, 6f, 7f)
+        };
 
-            matrix.Column1 = new Vector4(7f, 5f, 3f, 6f);
-            matrix.Column2 = new Vector4(3f, 4f, 5f, 9f);
-            matrix.Column3 = new Vector4(2f, 1f, 2f, 3f);
-            matrix.Column4 = new Vector4(4f, 5f, 6f, 7f);
+        return matrix;
+    }
 
-            return matrix;
-        }
-
-        private Viewport GetNormalViewport()
+    private static Viewport GetNormalViewport()
+    {
+        var viewport = new Viewport
         {
-            Viewport viewport = new Viewport();
+            Height = 7,
+            Width = 4,
+            X = 3,
+            Y = 6,
+            MaxDepth = 9f,
+            MinDepth = 1f
+        };
 
-            viewport.Height = 7;
-            viewport.Width = 4;
-            viewport.X = 3;
-            viewport.Y = 6;
-            viewport.MaxDepth = 9f;
-            viewport.MinDepth = 1f;
+        return viewport;
+    }
 
-            return viewport;
-        }
+    [Fact]
+    public void AllDataZero_Success()
+    {
+        _ = new Vector3(0f).Project(new Viewport(), GetEmptyMatrix(), GetEmptyMatrix(), GetEmptyMatrix());
+    }
 
-        /// <summary>
-        /// In this test all data (matrix, Viewimport and Vector3) is zero
-        /// </summary>
-        /// <returns></returns>
-        [Fact]
-        public async Task AllDataZero_Success()
-        {
-            //Act
-            var result = Vector3Ex.Project(new Vector3(0f), new Viewport(), GetEmptyMatrix(), GetEmptyMatrix(), GetEmptyMatrix());
+    [Fact]
+    public void VectorIsZero_True()
+    {
+        //Act
+        var result = new Vector3(0f).Project(new Viewport(), GetMatrixWithNormalColumns3(),
+            GetMatrixWithNormalColumns2(), GetMatrixWithNormalColumns1());
 
-            //Assert
-            Assert.NotNull(result);
-        }
-        #region Test vector is zero
-        [Fact]
-        public async Task VectorIsZero_True()
-        {
-            //Act
-            var result = Vector3Ex.Project(new Vector3(0f), new Viewport(), GetMatrixWithNormalColomns3(),
-                GetMatrixWithNormalColomns2(), GetMatrixWithNormalColomns1());
+        //Assert
+        Assert.True(result.IsZero);
+    }
 
-            //Assert
-            Assert.True(result.IsZero);
-        }
-        [Fact]
-        public async Task VectorIsZero_False()
-        {
-            //Act
-            var result = Vector3Ex.Project(new Vector3(0f), GetNormalViewport(), GetEmptyMatrix(), GetEmptyMatrix(), GetEmptyMatrix());
+    [Fact]
+    public void VectorIsZero_False()
+    {
+        //Act
+        var result = new Vector3(0f).Project(GetNormalViewport(), GetEmptyMatrix(), GetEmptyMatrix(), GetEmptyMatrix());
 
-            //Assert
-            Assert.False(result.IsZero);
-        }
-        #endregion
-        #region Test vector is normalized
-        [Fact]
-        public async Task IsNormalizedWithEmptyData_False()
-        {
-            //Act
-            var result = Vector3Ex.Project(new Vector3(0f), new Viewport(), new Matrix(), new Matrix(), new Matrix());
+        //Assert
+        Assert.False(result.IsZero);
+    }
 
-            //Assert
-            Assert.False(result.IsNormalized);
-        }
-        [Fact]
-        public async Task IsNormalizedWithNotEmptyData_False()
-        {
-            //Act
-            var result = Vector3Ex.Project(new Vector3(3f), GetNormalViewport(), GetMatrixWithNormalColomns3(),
-                GetMatrixWithNormalColomns2(), GetMatrixWithNormalColomns1());
+    [Fact]
+    public void IsNormalizedWithEmptyData_False()
+    {
+        //Act
+        var result = new Vector3(0f).Project(new Viewport(), new Matrix(), new Matrix(), new Matrix());
 
-            //Assert
-            Assert.False(result.IsNormalized);
-        }
-        #endregion
+        //Assert
+        Assert.False(result.IsNormalized);
+    }
 
-        [Fact]
-        public async Task NormalWithData_Success()
-        {
-            //Act
-            var result = Vector3Ex.Project(new Vector3(4f), GetNormalViewport(), GetMatrixWithNormalColomns2(), GetMatrixWithNormalColomns1(), GetMatrixWithNormalColomns3());
+    [Fact]
+    public void IsNormalizedWithNotEmptyData_False()
+    {
+        //Act
+        var result = new Vector3(3f).Project(GetNormalViewport(), GetMatrixWithNormalColumns3(),
+            GetMatrixWithNormalColumns2(), GetMatrixWithNormalColumns1());
 
-            //Assert
-            Assert.NotNull(result);
-        }
-        [Fact]
-        public async Task CorrectCalculationsWithNormalData_Success()
-        {
-            //Act
-            var result = Vector3Ex.Project(new Vector3(4f), GetNormalViewport(), GetMatrixWithNormalColomns1(),
-                GetMatrixWithNormalColomns2(), GetMatrixWithNormalColomns3());
+        //Assert
+        Assert.False(result.IsNormalized);
+    }
 
-            //Assert
-            Assert.True(result.X == 6.634325f && result.Y == 6.068153f && result.Z == 6.3136373f);
-        }
+    [Fact]
+    public void NormalWithData_Success()
+    {
+        _ = new Vector3(4f).Project(GetNormalViewport(), GetMatrixWithNormalColumns2(), GetMatrixWithNormalColumns1(), GetMatrixWithNormalColumns3());
+    }
+
+    [Fact]
+    public void CorrectCalculationsWithNormalData_Success()
+    {
+        const float tolerance = 0.0000001f;
+
+        var result = new Vector3(4f).Project(GetNormalViewport(), GetMatrixWithNormalColumns1(),
+            GetMatrixWithNormalColumns2(), GetMatrixWithNormalColumns3());
+
+        Assert.Equal(6.634325f, result.X, tolerance);
+        Assert.Equal(6.068153f, result.Y, tolerance);
+        Assert.Equal(6.3136373f, result.Z, tolerance);
     }
 }
