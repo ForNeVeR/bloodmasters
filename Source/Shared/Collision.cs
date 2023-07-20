38d0bd8d35b0aa4308f509c71c1ddf01f8917822
Bookuha <bookuha@gmail.com>
2023-07-20 18:41:07 +0300

    Fix ForNeVeR/bloodmasters#32

--- Source/Shared/Collision.cs
+++ Source/Shared/Collision.cs
@@ -12,7 +12,7 @@ using CodeImp;
 
 namespace CodeImp.Bloodmasters
 {
-	public abstract class Collision : IComparable
+	public abstract class Collision : IComparable<Collision>
 	{
 		protected bool collide;
 		protected float distance;
@@ -20,7 +20,7 @@ namespace CodeImp.Bloodmasters
 		protected Vector2D objslidevec;
 		protected Vector2D objbouncevec;
 		protected object collideobj;
-		
+
 		// Properties
 		public bool IsColliding { get { return collide; } }
 		public float Distance { get { return distance; } }
@@ -28,35 +28,36 @@ namespace CodeImp.Bloodmasters
 		public Vector2D ObjSlideVec { get { return objslidevec; } }
 		public Vector2D ObjBounceVec { get { return objbouncevec; } }
 		public object CollideObj{ get { return collideobj; } }
-		
+
 		// Constructor
 		public Collision()
 		{
 		}
-		
+
 		// This compares one collision with another
-		public int CompareTo(object obj)
+		public int CompareTo(Collision other)
 		{
-			// Get the proper object reference
-			Collision c2 = (Collision)obj;
-			
+
+            // If other is not a valid object reference, this instance is greater.
+            if (other is null) return 1;
+
 			// Return 1 if this collides earlier, otherwise return -1
-			if(this.distance > c2.distance) return 1; else return -1;
-		}
-		
+            return distance.CompareTo(other.distance);
+        }
+
 		// This makes updates to the subject position
 		// and returns the new collision
 		public abstract Collision Update(Vector3D objpos, Vector2D objvec);
-		
+
 		// Response vectors
 		public abstract Vector2D GetBounceVector();
 		public abstract Vector2D GetSlideVector();
-		
+
 		#if CLIENT
-		
+
 		// This renders the collision
 		public abstract void Render();
-		
+
 		#endif
 	}
 }
