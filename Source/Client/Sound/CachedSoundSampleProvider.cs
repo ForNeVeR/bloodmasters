ab4e85f30935301cf7c956b6504869da75869460
Руслан <@DESKTOP-9QBONLB>
2023-07-22 17:43:40 +0600

    Fire and Forget Audio Playback with NAudio

---
+++ Source/Client/Sound/CachedSoundSampleProvider.cs
@@ -0,0 +1,27 @@
+using System;
+using NAudio.Wave;
+
+namespace FireAndForgetAudioSample
+{
+    class CachedSoundSampleProvider : ISampleProvider
+    {
+        private readonly CachedSound cachedSound;
+        private long position;
+
+        public CachedSoundSampleProvider(CachedSound cachedSound)
+        {
+            this.cachedSound = cachedSound;
+        }
+
+        public int Read(float[] buffer, int offset, int count)
+        {
+            var availableSamples = cachedSound.AudioData.Length - position;
+            var samplesToCopy = Math.Min(availableSamples, count);
+            Array.Copy(cachedSound.AudioData, position, buffer, offset, samplesToCopy);
+            position += samplesToCopy;
+            return (int)samplesToCopy;
+        }
+
+        public WaveFormat WaveFormat { get { return cachedSound.WaveFormat; } }
+    }
+}
